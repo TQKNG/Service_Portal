2c96edf4159031ea80a830a8395e3dd224ee26d0
Tran Quoc Khanh Nguyen <khanhnguyen.trq@gmail.com>
2024-05-01 23:04:34 -0400

    Refactor Calendar Picker from ScheduleForm.js

--- client/src/components/admin/schedule/ScheduleForm.js
+++ client/src/components/admin/schedule/ScheduleForm.js
@@ -11,9 +11,7 @@ import {
 } from "../../../actions/admin";
 import Alert from "../../layouts/Alert";
 import PropTypes from "prop-types";
-import Badge from "react-bootstrap/Badge";
-import { DatePicker } from "antd";
-import moment from "moment";
+import DatePicker from "../../layouts/CalendarPicker";
 
 const ScheduleForm = ({
   schedule,
@@ -135,7 +133,7 @@ const ScheduleForm = ({
   // Populate the location list on robot change
   useEffect(() => {
     if (formData.Robot !== null) {
-      loadLocationsList({ userID: formData.Robot });
+      loadLocationsList({ userID: parseInt(formData.Robot) });
     }
   }, [formData.Robot]);
 
@@ -242,58 +240,7 @@ const ScheduleForm = ({
         {/* Select date time */}
         <div className="mb-3">
           <div className="txt-primary">Select start time</div>
-          <DatePicker
-            allowClear={false}
-            className="form-control rounded"
-            style={{ width: "20%" }}
-            format="YYYY-MM-DD hh:mm A"
-            renderExtraFooter={() => (
-              <div type="primary" className="w-100 p-4 text-center" size="large">
-                {/* Message */}
-                <div className="txt-primary">Already booked please select another time slot</div>
-                {/* Time slots */}
-                <div className="w-100 d-flex justify-content-center gap-3">
-                  <Badge pill className="w-15" bg={`danger`}>
-                    12:00 PM
-                  </Badge>
-                  <Badge pill className="w-15" bg={`success`}>
-                    5:00 PM
-                  </Badge>
-                </div>
-              </div>
-            )}
-            showTime={{
-              format: "hh:mm A",
-            }}
-            dateRender={(current) => {
-              const style = {};
-
-              // Customize the status of the calendar
-              if ([1, 3, 5].includes(current.date())) {
-                style.backgroundColor = "red";
-              }
-
-              return (
-                <div className="ant-picker-cell-inner" style={style}>
-                  {current.date()}
-                </div>
-              );
-            }}
-            disabledDate={(current) => {
-              // Can not select days before today
-              return current && current < moment().startOf("day");
-            }}
-            // disabledTime={(current) => {
-            //   if (current && current.date() === moment().date()) {
-            //     return {
-            //       disabledHours: () => range(0, moment().hours()),
-            //       disabledMinutes: () => range(0, moment().minutes()),
-            //       disabledSeconds: () => range(0, moment().seconds()),
-            //     };
-            //   }
-            //   return {};
-            // }}
-          />
+         <DatePicker/>
         </div>
 
         {/* Select duration*/}
---
+++ client/src/components/layouts/CalendarPicker.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Badge } from "react-bootstrap";
+import { DatePicker } from "antd";
+import moment from "moment";
+
+const CalendarPicker = () => {
+  return (
+    <>
+      <DatePicker
+        allowClear={false}
+        className="form-control rounded"
+        style={{ width: "20%" }}
+        format="YYYY-MM-DD hh:mm A"
+        renderExtraFooter={() => (
+          <div type="primary" className="w-100 p-4 text-center" size="large">
+            {/* Message */}
+            <div className="txt-primary">
+              Already booked please select another time slot
+            </div>
+            {/* Time slots */}
+            <div className="w-100 d-flex justify-content-center gap-3">
+              <Badge pill className="w-15" bg={`danger`}>
+                12:00 AM
+              </Badge>
+              <Badge pill className="w-15" bg={`danger`}>
+                12:00 AM
+              </Badge>
+              <Badge pill className="w-15" bg={`success`}>
+                5:00 PM
+              </Badge>
+            </div>
+          </div>
+        )}
+        showTime={{
+          format: "hh:mm A",
+        }}
+        // dateRender={(current) => {
+        //   const style = {};
+
+        //   // Customize the status of the calendar
+        //   if ([1, 3, 5].includes(current.date())) {
+        //     style.backgroundColor = "red";
+        //   }
+
+        //   return (
+        //     <div className="ant-picker-cell-inner" style={style}>
+        //       {current.date()}
+        //     </div>
+        //   );
+        // }}
+        disabledDate={(current) => {
+          // Can not select days before today
+          return current && current < moment().startOf("day");
+        }}
+        // disabledTime={(current) => {
+        //   if (current && current.date() === moment().date()) {
+        //     return {
+        //       disabledHours: () => range(0, moment().hours()),
+        //       disabledMinutes: () => range(0, moment().minutes()),
+        //       disabledSeconds: () => range(0, moment().seconds()),
+        //     };
+        //   }
+        //   return {};
+        // }}
+      />
+    </>
+  );
+};
+
+export default CalendarPicker;
