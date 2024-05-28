8e4f8b65ebadc3232da0b1539af801b67cc7e89d
Khanh Ng <83807651+TQKNG@users.noreply.github.com>
2024-05-28 11:43:41 -0400

    Activate visits persist to db and send email functionality

--- controllers/Reception.js
+++ controllers/Reception.js
@@ -27,116 +27,116 @@ exports.addReception = async (req, res) => {
         var SignInOutTime = moment.utc().format();
       }
 
-      // // // Look for latest visit record
-      // const pool = await poolPromise;
-      // let latestVisitRecord = await pool
-      //   .request()
-      //   .input("firstName", FirstName)
-      //   .input("lastName", LastName)
-      //   .input("phoneNumber", PhoneNumber)
-      //   .input("latestVisit", 1)
-      //   .execute("dbo.Visits_Load");
+      // // Look for latest visit record
+      const pool = await poolPromise;
+      let latestVisitRecord = await pool
+        .request()
+        .input("firstName", FirstName)
+        .input("lastName", LastName)
+        .input("phoneNumber", PhoneNumber)
+        .input("latestVisit", 1)
+        .execute("dbo.Visits_Load");
 
-      // // First time visit then insert new record
-      // if (latestVisitRecord.recordset.length === 0) {
-      //   await pool
-      //     .request()
-      //     .input("firstName", FirstName)
-      //     .input("lastName", LastName)
-      //     .input("phoneNumber", PhoneNumber)
-      //     .input("signInDate", SignInOutTime)
-      //     .input("homeAreas", JSON.stringify(HomeAreas))
-      //     .input("scheduledVisit", ScheduledVisit)
-      //     .input("purpose", Purpose)
-      //     .input("residentName", ResidentName)
-      //     .input("firstVisit", FirstVisit)
-      //     .input("sicknessSymptom", SicknessSymptom)
-      //     .input("acknowledgement", Acknowledgement)
-      //     .input("adminOffices", DepartmentVisit)
-      //     .execute("dbo.Visits_Insert");
-      // }
-      // // If there is a record, check if the user has signed in or out
-      // else if (
-      //   latestVisitRecord.recordset[0].signInDate &&
-      //   latestVisitRecord.recordset[0].signOutDate
-      // ) {
-      //   await pool
-      //     .request()
-      //     .input("firstName", FirstName)
-      //     .input("lastName", LastName)
-      //     .input("phoneNumber", PhoneNumber)
-      //     .input("signInDate", SignInOutTime)
-      //     .input("homeAreas", JSON.stringify(HomeAreas))
-      //     .input("scheduledVisit", ScheduledVisit)
-      //     .input("purpose", Purpose)
-      //     .input("residentName", ResidentName)
-      //     .input("firstVisit", FirstVisit)
-      //     .input("sicknessSymptom", SicknessSymptom)
-      //     .input("acknowledgement", Acknowledgement)
-      //     .input("adminOffices", DepartmentVisit)
-      //     .execute("dbo.Visits_Insert");
-      // } else {
-      //   if (
-      //     latestVisitRecord.recordset[0].signInDate &&
-      //     !latestVisitRecord.recordset[0].signOutDate
-      //   ) {
-      //     return res.status(200).json({
-      //       success: false,
-      //       error:
-      //         "You have not signed out yet. Please sign out before continue",
-      //     });
-      //   }
-      // }
+      // First time visit then insert new record
+      if (latestVisitRecord.recordset.length === 0) {
+        await pool
+          .request()
+          .input("firstName", FirstName)
+          .input("lastName", LastName)
+          .input("phoneNumber", PhoneNumber)
+          .input("signInDate", SignInOutTime)
+          .input("homeAreas", JSON.stringify(HomeAreas))
+          .input("scheduledVisit", ScheduledVisit)
+          .input("purpose", Purpose)
+          .input("residentName", ResidentName)
+          .input("firstVisit", FirstVisit)
+          .input("sicknessSymptom", SicknessSymptom)
+          .input("acknowledgement", Acknowledgement)
+          .input("adminOffices", DepartmentVisit)
+          .execute("dbo.Visits_Insert");
+      }
+      // If there is a record, check if the user has signed in or out
+      else if (
+        latestVisitRecord.recordset[0].signInDate &&
+        latestVisitRecord.recordset[0].signOutDate
+      ) {
+        await pool
+          .request()
+          .input("firstName", FirstName)
+          .input("lastName", LastName)
+          .input("phoneNumber", PhoneNumber)
+          .input("signInDate", SignInOutTime)
+          .input("homeAreas", JSON.stringify(HomeAreas))
+          .input("scheduledVisit", ScheduledVisit)
+          .input("purpose", Purpose)
+          .input("residentName", ResidentName)
+          .input("firstVisit", FirstVisit)
+          .input("sicknessSymptom", SicknessSymptom)
+          .input("acknowledgement", Acknowledgement)
+          .input("adminOffices", DepartmentVisit)
+          .execute("dbo.Visits_Insert");
+      } else {
+        if (
+          latestVisitRecord.recordset[0].signInDate &&
+          !latestVisitRecord.recordset[0].signOutDate
+        ) {
+          return res.status(200).json({
+            success: false,
+            error:
+              "You have not signed out yet. Please sign out before continue",
+          });
+        }
+      }
 
-      // // Configure email to be sent
-      // const settings = await pool
-      //   .request()
-      //   .input("keyword", "AdminOffices")
-      //   .execute("dbo.Settings_Load");
+      // Configure email to be sent
+      const settings = await pool
+        .request()
+        .input("keyword", "AdminOffices")
+        .execute("dbo.Settings_Load");
 
-      // if (settings.recordset.length > 0) {
-      //   const adminOffices = JSON.parse(settings.recordset[0].valueStr);
-      //   let ccEmail = "";
-      //   let options = {};
+      if (settings.recordset.length > 0) {
+        const adminOffices = JSON.parse(settings.recordset[0].valueStr);
+        let ccEmail = "";
+        let options = {};
 
-      //   // Check first: Scenarios 3: Sickness symptom
-      //   if (SicknessSymptom === "true") {
-      //     options.emailType = 3;
-      //     options.visitorName = `${FirstName} ${LastName}`;
-      //     options.phoneNumber = PhoneNumber;
-      //   } else {
-      //     // Scenarios 1: Select admin office
-      //     if (DepartmentVisit !== "") {
-      //       office = adminOffices.find(
-      //         (office) => office.title === DepartmentVisit
-      //       );
+        // Check first: Scenarios 3: Sickness symptom
+        if (SicknessSymptom === "true") {
+          options.emailType = 3;
+          options.visitorName = `${FirstName} ${LastName}`;
+          options.phoneNumber = PhoneNumber;
+        } else {
+          // Scenarios 1: Select admin office
+          if (DepartmentVisit !== "") {
+            office = adminOffices.find(
+              (office) => office.title === DepartmentVisit
+            );
 
-      //       ccEmail = office.email;
-      //       options.emailType = 1;
-      //       options.deptName = DepartmentVisit;
-      //       options.cc = ccEmail;
-      //       options.visitorName = `${FirstName} ${LastName}`;
-      //       options.phoneNumber = PhoneNumber;
-      //     }
+            ccEmail = office.email;
+            options.emailType = 1;
+            options.deptName = DepartmentVisit;
+            options.cc = ccEmail;
+            options.visitorName = `${FirstName} ${LastName}`;
+            options.phoneNumber = PhoneNumber;
+          }
 
-      //     // Scenarios 2: First visit  and Caregiver/general visitor
-      //     if (
-      //       FirstVisit==='true' &&
-      //       (Purpose === "Caregiver" || Purpose === "General Visitor")
-      //     ) {
-      //       options.emailType = 2;
-      //       options.visitorName = `${FirstName} ${LastName}`;
-      //       options.phoneNumber = PhoneNumber;
-      //     }
-      //   }
+          // Scenarios 2: First visit  and Caregiver/general visitor
+          if (
+            FirstVisit==='true' &&
+            (Purpose === "Caregiver" || Purpose === "General Visitor")
+          ) {
+            options.emailType = 2;
+            options.visitorName = `${FirstName} ${LastName}`;
+            options.phoneNumber = PhoneNumber;
+          }
+        }
         res.status(200).json({ success: true });
 
-        // Send email to department'
-        // if(options.emailType !== 0){
-        //   await sendEmailToDept(options);
-        // }
+        //Send email to department'
+        if(options.emailType !== 0){
+          await sendEmailToDept(options);
+        }
       }
-    // }
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, error: "Server Error" });
